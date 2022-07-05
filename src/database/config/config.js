1c4b2faf1e459b4625e71dba6ec1d6bddadf0c92
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-07-05 14:09:28 -0300

    fixed

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "Password123#@!",
-    database: "db_paginasbellas",
+    username:process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: "mysql",
     port : process.env.DB_PORT
